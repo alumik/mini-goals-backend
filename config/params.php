03ac90ef32b22cc280f2bad10f6200c1e9bae7c4
Zhong Zhenyu <nczzy1997@gmail.com>
2020-10-06 14:23:28 +0800

    移除测试用 appid 和 secret

--- config/params.php
+++ config/params.php
@@ -4,6 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'wx_appid' => 'wxd0adda4e462ea087',
-    'wx_secret' => 'e5a805582e50c56bedc8ed581facac5a',
+    'wx_appid' => '',
+    'wx_secret' => '',
 ];
